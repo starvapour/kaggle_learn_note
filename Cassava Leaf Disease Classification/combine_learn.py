13243b52853bd8d71e2e89682431a3680ab7d04b
starvapour <starvapour@126.com>
2021-01-05 10:08:47 +0800

    add

---
+++ Cassava Leaf Disease Classification/combine_learn.py
@@ -0,0 +1,300 @@
+import cv2
+import torch
+import os
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as toptim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import transforms, models
+from PIL import Image
+#import pydicom
+import time
+from efficientnet_pytorch import EfficientNet
+
+
+
+# ------------------------------------config------------------------------------
+train_csv = "train.csv"
+train_image = "train_images/"
+preprocessed_image = "train_preprocessed_images/"
+
+# do preprocessing or not
+do_preprocessing = False
+
+# continue train from old model
+from_old_model = True
+
+# Train config
+# learning rate
+learning_rate = 0.0001
+# max epoch
+epochs = 10
+# batch size
+batchSize = 16
+'''
+# model save each step
+model_save_step = 10
+'''
+
+# Use how many data of the dataset for val
+proportion_of_val_dataset = 0.1
+
+# output path
+log_name = "log.txt"
+
+# used for debug, set -1 to use all data
+sampler_len = -1
+
+# record best val acc with (epoch_num, acc)
+best_val_acc = (-1, 0)
+
+# ------------------------------------preprocess------------------------------------
+def preprocess(img_name, ori_path, target_path):
+    img = cv2.imread(ori_path + img_name, 1)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (256, 256))
+    cv2.imwrite(target_path + img_name, img)
+
+
+
+# ------------------------------------dataset------------------------------------
+# create dataset
+class Leaf_train_Dataset(Dataset):
+    def __init__(self, data_csv, img_path, transform):
+        # get lists
+        self.csv = data_csv
+        self.img_path = img_path
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image_id = self.csv.loc[index, 'image_id']
+        label = self.csv.loc[index, 'label']
+        img = Image.open(self.img_path + image_id)
+        img = self.transform(img)
+        return img, label
+
+    def __len__(self):
+        return len(self.csv)
+
+# ------------------------------------model------------------------------------
+# the net model
+class combine_net(nn.Module):
+
+    def __init__(self):
+        super(combine_net, self).__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2000, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, features):
+        # image
+        features = features.view(features.shape[0], -1)
+        output = self.classifier(features)
+
+        return output
+
+# ------------------------------------train------------------------------------
+def train(net, train_loader, criterion, optimizer, epoch, device, log, efficient_net, res_net):
+    # start train
+    runningLoss = 0
+    loss_count = 0
+
+    batch_num = len(train_loader)
+    for index, (imgs, labels) in enumerate(train_loader):
+        # send data to device
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        with torch.no_grad():
+            data = torch.cat((efficient_net(imgs),res_net(imgs)),1)
+
+        # zero grad
+        optimizer.zero_grad()
+
+        # forward
+        output = net(data)
+
+        # calculate loss
+        #output = output.reshape(target.shape)
+        loss = criterion(output, labels)
+
+        runningLoss += loss.item()
+        loss_count += 1
+
+        # calculate gradients.
+        loss.backward()
+
+        # reduce loss
+        optimizer.step()
+
+        # print loss
+        # print(index)
+        if (index + 1) % 100 == 0:
+            print("Epoch: %2d, Batch: %4d / %4d, Loss: %.3f" % (epoch + 1, index + 1, batch_num, loss.item()))
+
+    avg_loss = runningLoss / loss_count
+    print("For Epoch: %2d, Average Loss: %.3f" % (epoch + 1, avg_loss))
+    log.write("For Epoch: %2d, Average Loss: %.3f" % (epoch + 1, avg_loss) + "\n")
+
+# ------------------------------------val------------------------------------
+def val(net, val_loader, criterion, optimizer, epoch, device, log, val_len, train_start, efficient_net, res_net):
+    # val after each epoch
+    net.eval()
+
+    with torch.no_grad():
+        total_len = 0
+        correct_len = 0
+        global best_val_acc
+        for index, (imgs, labels) in enumerate(val_loader):
+            imgs, labels = imgs.to(device), labels.to(device)
+            data = torch.cat((efficient_net(imgs), res_net(imgs)), 1)
+            output = net(data)
+            pred = output.argmax(dim=1, keepdim=True).reshape(labels.shape)
+            assessment = torch.eq(pred, labels)
+            total_len += len(pred)
+            correct_len += int(assessment.sum())
+        accuracy = correct_len / total_len
+        print("Start val:")
+        print("accuracy:", accuracy)
+        log.write("accuracy: " + str(accuracy) + "\n")
+
+        # if have better acc, save model, only keep the best model in epochs
+        if accuracy > best_val_acc[1]:
+            # save model
+            best_val_acc = (epoch+1, accuracy)
+            torch.save(net.state_dict(), "save_model.pth")
+            print("Model saved in epoch "+str(epoch+1)+", acc: "+str(accuracy)+".")
+            log.write("Model saved in epoch "+str(epoch+1)+", acc: "+str(accuracy)+".\n")
+
+
+    # print time pass after each epoch
+    current_time = time.time()
+    pass_time = int(current_time - train_start)
+    time_string = str(pass_time // 3600) + " hours, " + str((pass_time % 3600) // 60) + " minutes, " + str(
+        pass_time % 60) + " seconds."
+    print("Time pass:", time_string)
+    print()
+    log.write("Time pass: " + time_string + "\n\n")
+
+# ------------------------------------main------------------------------------
+# main
+def main():
+    # if GPU is availale, use GPU
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print("Use " + str(device))
+
+    # create dataset
+    original_csv_data = pd.read_csv(train_csv)
+    print("length of original dataset is", len(original_csv_data))
+    log.write("length of original dataset is " + str(len(original_csv_data)) + "\n")
+
+    # preprocessing steps
+    data_transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    original_dataset = Leaf_train_Dataset(original_csv_data, preprocessed_image, transform = data_transform)
+
+    # if need, use smaller dataset for fast debug
+    if sampler_len != -1:
+        original_dataset, drop_dataset = torch.utils.data.random_split(original_dataset, [sampler_len, len(
+            original_dataset) - sampler_len])
+
+    print("length of selected dataset is", len(original_dataset))
+    print()
+    log.write("length of selected dataset is " + str(len(original_dataset)) + "\n\n")
+
+    if do_preprocessing == True:
+        if not os.path.exists(preprocessed_image):
+            os.makedirs(preprocessed_image)
+        print("Start pre-processing:")
+        log.write("Start pre-processing:\n")
+        length = len(original_csv_data['image_id'])
+        count = 0
+        for img_name in original_csv_data['image_id']:
+            if count % 1000 == 0:
+                print("preprocessing "+str(count)+"/"+str(length))
+            preprocess(img_name, train_image, preprocessed_image)
+            count += 1
+
+    # split dataset, get train and val
+    train_len = int((1 - proportion_of_val_dataset) * len(original_dataset))
+    val_len = len(original_dataset) - train_len
+
+    train_dataset, val_dataset = torch.utils.data.random_split(original_dataset, [train_len, val_len])
+
+    print("Start random split:")
+    print("length of train dataset is", len(train_dataset))
+    log.write("length of train dataset is " + str(len(train_dataset)) + "\n")
+    print("length of val dataset is", len(val_dataset))
+    log.write("length of val dataset is " + str(len(val_dataset)) + "\n\n")
+    print()
+    print("Start training:")
+
+    # net model
+    efficient_net = EfficientNet.from_name('efficientnet-b0')
+    efficient_net._fc.out_features = 10
+    efficient_net = efficient_net.to(device)
+    efficient_net.load_state_dict(torch.load("effcientnet_model.pth"))
+
+    res_net = torchvision.models.resnet50(pretrained=False)
+    res_net.fc.out_features = 10
+    res_net = res_net.to(device)
+    res_net.load_state_dict(torch.load("resnet_model.pth"))
+
+    net = combine_net()
+    net = net.to(device)
+    # if true, continue train old model
+    if from_old_model:
+        net.load_state_dict(torch.load("save_model.pth"))
+
+    # create dataloader
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batchSize, shuffle=True)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=batchSize, shuffle=True)
+
+    # loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # create optimizer
+    optimizer = toptim.SGD(net.parameters(), lr=learning_rate)
+    #optimizer = toptim.Adam(net.parameters(), lr=learning_rate)
+
+    train_start = time.time()
+
+    for epoch in range(epochs):
+        '''
+        # change lr by epoch
+        adjust_learning_rate(optimizer, epoch)
+        '''
+
+        # start train
+        train(net, train_loader, criterion, optimizer, epoch, device, log, efficient_net, res_net)
+
+        # start val
+        val(net, val_loader, criterion, optimizer, epoch, device, log, val_len, train_start, efficient_net, res_net)
+
+    print("Final saved model is epoch "+str(best_val_acc[0])+", acc: "+str(best_val_acc[1])+".")
+    log.write("Final saved model is epoch "+str(best_val_acc[0])+", acc: "+str(best_val_acc[1])+"\n")
+
+    print("Done.")
+    log.write("Done.\n")
+
+
+if __name__ == '__main__':
+    with open(log_name, 'w') as log:
+        main()
+
+
+
+
---
+++ Cassava Leaf Disease Classification/combine_pred.py
@@ -0,0 +1,134 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, models
+from PIL import Image
+import torchvision
+import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+
+
+
+# ------------------------------------config------------------------------------
+test_path = "test_images/"
+model_path = "save_model.pth"
+output_path = ""
+
+# Test config
+# batch size
+batchSize = 16
+
+
+# the net model
+class combine_net(nn.Module):
+
+    def __init__(self):
+        super(combine_net, self).__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(2000, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, features):
+        # image
+        features = features.view(features.shape[0], -1)
+        output = self.classifier(features)
+
+        return output
+
+# ------------------------------------dataset------------------------------------
+# create dataset
+class Leaf_test_Dataset(Dataset):
+    def __init__(self, file_list, test_path, transform):
+        # get lists
+        self.file_list = file_list
+        self.test_path = test_path
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image_path = self.test_path + self.file_list[index]
+        img = Image.open(image_path)
+        img = self.transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.file_list)
+
+# ------------------------------------main------------------------------------
+# main
+def main():
+    # if GPU is availale, use GPU
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print("Use " + str(device))
+
+    # create dataset
+    file_list = None
+    for path, dirs, files in os.walk(test_path, topdown=False):
+        file_list = list(files)
+
+    # preprocessing steps
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    test_dataset = Leaf_test_Dataset(file_list, test_path, transform)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batchSize)
+
+    print("Start testing:")
+
+    # net model
+    # net model
+    efficient_net = EfficientNet.from_name('efficientnet-b0')
+    efficient_net._fc.out_features = 10
+    efficient_net = efficient_net.to(device)
+    efficient_net.load_state_dict(torch.load("effcientnet_model.pth"))
+
+    res_net = torchvision.models.resnet50(pretrained=False)
+    res_net.fc.out_features = 10
+    res_net = res_net.to(device)
+    res_net.load_state_dict(torch.load("resnet_model.pth"))
+
+    net = combine_net()
+    net = net.to(device)
+    net.load_state_dict(torch.load("save_model.pth"))
+
+    efficient_net.eval()
+    res_net.eval()
+    net.eval()
+
+    result = []
+
+    with torch.no_grad():
+        batch_num = len(test_loader)
+        for index, image in enumerate(test_loader):
+            image = image.to(device)
+            data = torch.cat((efficient_net(image), res_net(image)), 1)
+            output = net(data)
+            pred = output.argmax(dim=1, keepdim=True)
+
+            pred = pred.view(pred.shape[0], -1)
+            result = result + list(map(lambda x:int(x), pred))
+
+            if (index + 1) % 10 == 0:
+                print("Batch: %4d / %4d" % (index + 1, batch_num))
+
+    pred_result = pd.concat([pd.DataFrame(file_list, columns=['image_id']), pd.DataFrame(result, columns=['label'])], axis=1)
+    pred_result.to_csv(output_path + "submission.csv", index=False, sep=',')
+
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
