65a7b3e99983bf97b744e4f6e064474a4d36e90c
starvapour <starvapour@126.com>
2021-01-06 10:50:24 +0800

    add VerticalFlip

--- Cassava Leaf Disease Classification/efficientnet_learn.py
+++ Cassava Leaf Disease Classification/efficientnet_learn.py
@@ -171,6 +171,7 @@ def main():
         #transforms.Resize(250),
         transforms.RandomCrop(224),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
         transforms.ToTensor(),
         #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
